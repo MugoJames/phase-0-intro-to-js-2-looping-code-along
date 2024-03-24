0bf4d2d436063eef15ed0e879451ee1c5dded3a6
James <jamonjoroge816@gmail.com>
2024-03-24 16:03:07 +0300

    JS 2 looping code along

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+
+
+function writeCards(namesArray, eventName) {
+    let messages = [];
+
+    for (let i = 0; i < namesArray.length; i++) {
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+
+      messages.push(message);
+
+      debugger;
+    }
+
+    return messages;
+  }
+
+  function countDown(startingNumber) {
+
+      
+      if (typeof startingNumber !== 'number' || startingNumber < 1 || !Number.isInteger(startingNumber)) {
+          console.log("Please provide a positive integer as input.");
+          return;
+      }
+
+
+      while (startingNumber >= 0) {
+          console.log(startingNumber);
+          startingNumber--;
+      }
+
+  }
+
+  // Example usage:
+  countDown(5);
